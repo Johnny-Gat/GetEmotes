46b68bd7db3a175e2402ccea350ec39ad2df4d3f
Johnny Gat <55049724+Johnny-Gat@users.noreply.github.com>
2022-03-06 15:05:24 +0300

    Create GetEmotes.lua

---
+++ GetEmotes/GetEmotes.lua
@@ -0,0 +1,24 @@
+numEmo = 1
+numCmd = 1
+currNum = 0
+
+local function OnEvent(self, event, ...)
+	if event == "PLAYER_STARTED_MOVING" then
+		if numCmd == 1 then
+			SendChatMessage(numEmo, 'EMOTE')
+			numCmd = numCmd + 1
+		elseif numCmd == 2 then
+			DoEmote(_G['EMOTE'..numEmo..'_TOKEN'], 'none')
+			numCmd = numCmd + 1
+		elseif numCmd == 3 then
+			DoEmote(_G['EMOTE'..numEmo..'_TOKEN'])
+			numCmd = 1
+			numEmo = numEmo + 1
+		end
+	end
+
+end
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_STARTED_MOVING")
+f:SetScript("OnEvent", OnEvent)
